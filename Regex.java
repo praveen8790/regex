e4bf1fcf46e39b210eca3f1eaa5322660332b9fa
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-31 23:07:11 +0530

    handled exceptions

--- Regex.java
+++ Regex.java
@@ -3,45 +3,51 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public boolean regexCompiler(String regexp, String expr){
-        Pattern compile = Pattern.compile(regexp);
-        Matcher matcher = compile.matcher(expr);
-        boolean matches = matcher.matches();
-        return matches;
+    public static boolean regexCompiler(String regexp, String expr) throws RegexMismatchException {
+        try {
+            Pattern compile = Pattern.compile(regexp);
+            Matcher matcher = compile.matcher(expr);
+            boolean matches = matcher.matches();
+            System.out.println(expr + matches);
+            return matches;
+        }
+        catch (Exception exception) {
+            throw new RegexMismatchException("enter valid input");
+        }
     }
     public String firstName(){
         return "^[A-Z][a-z]{3,}$";
     }
     public String lastName(){
-        return "^[A-Z][a-z]{3,}$";
+        return "^[A-Z][a-z]{2,}$";
     }
 
     public String email(){
-        return "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9-](?:[a-z-]*[a-z])?\\.)+[a-z]{3,}(?:[a-z-]*[a-z]{3,})?";
+        return "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9-](?:[a-z-]*[a-z])?\\.)+[a-z]{2,}(?:[a-z-]*[a-z]{3,})?";
     }
     public String mobileNumber(){
-        return "^(?:(?:\\+|0{0,2})91(\\s*|[\\-])?|[0]?)?([6789]\\d{2}([ -]?)\\d{3}([ -]?)\\d{4})$";
+        return "^(?:(?:\\+|0{0,2})91(\\s*|[\\-])?|[0]?)?([6789]{2}([0-9]{8}))$";
     }
     public String password(){
-        return "(?=.*[!@#$%^&*(),.-_])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
+        return "(?=.*[!@#$%^&*(),.-_])(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}$";
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RegexMismatchException {
         Regex regex = new Regex();
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter first name");
         String firstname = scanner.next();
-        regex.regexCompiler(regex.firstName(),firstname);
+        Regex.regexCompiler(regex.firstName(),firstname);
+        System.out.println("enter last name");
         String lastname = scanner.next();
-        regex.regexCompiler(regex.lastName(),lastname);
+        Regex.regexCompiler(regex.lastName(),lastname);
+        System.out.println("enter email");
         String email = scanner.next();
-        regex.regexCompiler(regex.email(),email);
-        String mobilenumber = scanner.next();
-        regex.regexCompiler(regex.mobileNumber(),mobilenumber);
+        Regex.regexCompiler(regex.email(),email);
+        System.out.println("enter mobile number");
+        String mobilenumber = scanner.nextLine();
+        Regex.regexCompiler(regex.mobileNumber(),mobilenumber);
+        System.out.println("enter password");
         String password = scanner.next();
-        regex.regexCompiler(regex.password(), password);
-
-
-
-
+        Regex.regexCompiler(regex.password(), password);
     }
 }
---
+++ RegexMismatchException.java
@@ -0,0 +1,6 @@
+public class RegexMismatchException extends Throwable{
+
+    public RegexMismatchException(String message) {
+        super(message);
+    }
+}
